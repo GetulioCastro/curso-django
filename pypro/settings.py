c78d830f0414215b78a7ad2dee3bf8720c1b7615
GetulioCastro <augustoge71@gmail.com>
2020-10-14 19:36:51 -0300

    Extraída nova chave secreta randomicamente

--- pypro/settings.py
+++ pypro/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'u$2_iol!yikk=7^im3w@n=^@_7t#*xag9j#%ttqd&*b7-8rwb-'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
